74787f8cc5ffcda6626972751e8fca0bb1c4cb82
Rosina Bignall <rosina@rosinabignall.com>
2015-03-21 08:29:42 -0700

    Add routing

--- routing/app.js
+++ routing/app.js
@@ -1,5 +1,24 @@
 (function() {
-    var app = angular.module('ranch', []);
+    var app = angular.module('ranch', ['ngRoute']);
+
+    app.config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/animals', {
+                templateUrl: 'animals.html',
+                controller: 'AnimalsController'
+            })
+            .when('/equipment', {
+                templateUrl: 'equipment.html',
+                controller: 'EquipmentController'
+            })
+            .when('/tasks', {
+                templateUrl: 'tasks.html',
+                controller: 'TasksController'
+            })
+            .otherwise({
+                redirectTo: '/animals'
+            });
+    }]);
     
     app.controller('RanchController', ['$http', function($http){
         var ranch = this;
@@ -8,6 +27,15 @@
             ranch.animals = data;
         });
     }]);
+
+    app.controller('AnimalsController', function() {
+    });
+
+    app.controller('EquipmentController', function() {
+    });
+
+    app.controller('TasksController', function() {
+    });
     
     app.directive('animalDescription', function() {
         return {
